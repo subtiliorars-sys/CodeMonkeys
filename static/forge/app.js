561f01af3e11b8197296fecb6cb13fe84f9a7ede
Daniel Read <subtilior.ars@gmail.com>
2026-07-16 09:20:47 -0400

    feat: auto-remember username on login form

--- static/forge/app.js
+++ static/forge/app.js
@@ -108,6 +108,9 @@ function hideAll() {
 function showLogin() {
   hideAll();
   $("view-login").classList.remove("hidden");
+  // Pre-fill the last username
+  const lastUser = localStorage.getItem("cm_last_user");
+  if (lastUser) $("lg-user").value = lastUser;
   FeedbackFab.syncWithAuthScreen?.();
   api("/api/registration-status").then(d => {
     if (d.open) $("lg-toggle").classList.remove("hidden");
@@ -196,6 +199,8 @@ function saveAuth(d) {
   localStorage.setItem("cm_token", d.token);
   localStorage.setItem("cm_username", d.username);
   localStorage.setItem("cm_role", d.role);
+  // Remember the last username for login form pre-fill
+  localStorage.setItem("cm_last_user", d.username);
 }
 
 $("lg-toggle").onclick = () => {
